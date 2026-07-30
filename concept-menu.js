ff44836b374e97393ff530def036be48c8c96391
pizko <onetwopic@yandex.ru>
2026-07-30 20:20:06 +0300

    Remove decorative medical icons

--- concept-menu.js
+++ concept-menu.js
@@ -5,22 +5,6 @@
     paletteStylesheet.href += (paletteStylesheet.href.includes('?') ? '&' : '?') + 'v=20260730-blob-layer';
   }
 
-  var conceptPage = document.querySelector('.concept-page');
-
-  if (conceptPage && !conceptPage.querySelector('.concept-medical-icons')) {
-    var medicalIcons = document.createElement('div');
-    medicalIcons.className = 'concept-medical-icons';
-    medicalIcons.setAttribute('aria-hidden', 'true');
-
-    ['🐾', '🩺', '🐾', '🔬', '🐾', '🐾'].forEach(function (icon) {
-      var mark = document.createElement('span');
-      mark.textContent = icon;
-      medicalIcons.appendChild(mark);
-    });
-
-    conceptPage.insertBefore(medicalIcons, conceptPage.firstChild);
-  }
-
   document.addEventListener("click", function (event) {
     document.querySelectorAll(".concept-side-menu[open]").forEach(function (menu) {
       if (!menu.contains(event.target)) {
